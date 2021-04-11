071a1ebf611b81d5c2db2bb6c877c15284ba6430
omar <omardiaa500@gmail.com>
2021-04-11 10:23:15 -0700

    modified proc struct

--- dist-test/proc.h
+++ dist-test/proc.h
@@ -35,6 +35,8 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
+  uint uid;                    //user id to track ownership
+  uint gid;                    //group id to track ownership
   uint sz;                     // Size of process memory (bytes)
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
