916e620cec55f2de4b05ed993bb0859f3dc074fb
Omar Eldesouky <om9@babbage.cs.pdx.edu>
2021-05-04 15:23:52 -0700

    Final Version before submission

--- my-p3-test3.c
+++ my-p3-test3.c
@@ -13,11 +13,15 @@ main(void)
       exit();
   }
   if(pid == 0){
+    printf(1,"Child process is sleeping for 10 seconds\n");
     sleep(10*TPS);
     printf(1,"Child process is exiting normally. \n");
     exit();
   }
-  printf(1,"Wait for the child to exit normally. \n");
+  sleep(5*TPS);
+  printf(1,"Wait for the child to exit normally. Sleeping for 20 seconds.\n");
+
+  sleep(20*TPS);
   
   printf(1,"Before Wait(). Sleeping for 5 seconds.\n");
   sleep(5*TPS);
